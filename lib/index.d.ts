d05eacf76e43c58b5cb5f93b585b2e0e6688ee0d
Yunus YAVUZ <yyavuz15@ford.com.tr>
2025-05-11 07:36:53 +0300

    Update project structure: added .npmignore to exclude development files, updated .gitignore to include 'dist', modified package.json to reflect new version and include 'dist/**/*', and changed TypeScript output directory to 'dist'. Removed obsolete files from 'lib' directory.

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { UpdatesProvider, useSelfHostedUpdates } from './provider';
-export { useUpdates } from './hooks';
-export { ReleaseChannel, Platform, SelfHostedUpdateConfig, UpdateAvailableEvent, UpdateNotAvailableEvent, UpdateErrorEvent, UpdateDownloadEvent, UpdateInstalledEvent, UpdateEvent, UpdateEventListener } from './types';
-import SelfHostedUpdates from './updates';
-export default SelfHostedUpdates;
